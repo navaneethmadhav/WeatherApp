1e9bc4b6a8d27cf13c48cf62b91098c1dca16e93
navaneethmadhav <madnav114@gmail.com>
2022-12-05 21:12:26 +0530

    Weather App

---
+++ script.js
@@ -0,0 +1,38 @@
+function Search() {
+    country = c_Name.value;  //country=india
+    console.log(country);
+
+
+    fetch(`https://api.weatherapi.com/v1/current.json?key=643def91cb154a5391350822220312&q=${country}`)
+        .then(res => res.json())
+    .then(data => displayData(data)) //2nd function call
+
+        .catch(error => {
+            alert('Failed to fetch country data')
+        })
+}
+
+//display data as card
+
+function displayData(weatherDetails) {
+    capital = weatherDetails['location'].country
+    region = weatherDetails['location'].region
+    temperature = weatherDetails['current'].temp_c
+    temperature_f = weatherDetails['current'].temp_f
+    humidity = weatherDetails['current'].humidity
+    let htmlData = `
+
+<div class="card-body">
+<h3 class="card-title">Weather Details</h3>
+</div>
+<h4>Weather in ${country} is ${temperature}°C</h4>
+<ul class="list-group list-group-flush">
+<li class="list-group-item">Capital: ${capital}</li>
+<li class="list-group-item">Region: ${region}</li>
+<li class="list-group-item">Temperature in Farenheit: <span style="font-size: 30px;">${temperature_f}°F</span></li>
+<li class="list-group-item">Humidity: <span style="font-size: 30px;">${humidity}%</span></li>
+</ul>
+
+`
+result.innerHTML = htmlData
+}
